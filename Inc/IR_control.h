dffe0640e95c62bd323bd2fb2fcddf6342497bf3
Grigory Pavlenko <Grigory.PAVLENKO@bachmann.info>
2017-07-18 13:27:56 +0200

    IR_Control created

---
+++ Inc/IR_control.h
@@ -0,0 +1,46 @@
+/*
+ * IR_control.h
+ *
+ *  Created on: 18.07.2017
+ *      Author: grpa
+ */
+
+#ifndef INC_IR_CONTROL_H_
+#define INC_IR_CONTROL_H_
+
+
+
+#endif /* INC_IR_CONTROL_H_ */
+
+#include <stm32l0xx_hal.h>
+
+#define SPACE_INTERVAL_US           19200
+
+#define ONE_LEVEL_INTERVAL_NUM      2
+#define ZERO_LEVEL_INTERVAL_NUM     1
+#define START_LEVEL_INTERVAL_NUM    4
+#define PAUSE_LEVEL_INTERVAL_NUM    1
+
+#define START_TIME_CORRECTION_US    10
+#define END_TIME_CORRECTION_US      10
+
+#define COMMAND_LENGTH              7
+#define ADDR_LENGTH                 5
+
+#define DEVICE_ADDRESS              1
+
+typedef struct {
+    uint32_t SendIrCommand;
+    uint32_t SendIrAddr;
+} IR_PacketTypeDef;
+
+typedef enum {
+    START_SYMBOL    = 0x01U,
+    ONE_SYMBOL      = 0x02U,
+    ZERO_SYMBOL     = 0x03U
+} IR_SymbolTypeDef;
+
+
+int SendIrData (uint32_t *DataToSend, uint32_t Length);
+int SendIrSymbol (IR_SymbolTypeDef Symbol);
+uint32_t CreasteIrPacket (IR_PacketTypeDef IR_Packet);
---
+++ Src/IR_Control.c
@@ -0,0 +1,74 @@
+/*
+ * IR_Control.c
+ *
+ *  Created on: 18.07.2017
+ *      Author: grpa
+ */
+#include "IR_control.h"
+
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim21;
+extern uint32_t DataTransfer;
+
+int SendIrData (uint32_t *DataToSend, uint32_t Length)
+{
+    uint32_t Data;
+    Data = *DataToSend;
+
+    SendIrSymbol (START_SYMBOL);
+    for (int i = 0; i < Length; i++){
+
+        if (Data & 0x00000001)
+        {
+            SendIrSymbol(ONE_SYMBOL);
+        }
+        else{
+            SendIrSymbol(ZERO_SYMBOL);
+        }
+        Data = Data >> 1;
+    }
+    return 0;
+}
+
+int SendIrSymbol (IR_SymbolTypeDef Symbol)
+{
+    // Start transfer symbol
+    switch (Symbol){
+    case START_SYMBOL:
+        DataTransfer = START_LEVEL_INTERVAL_NUM;
+        break;
+    case ONE_SYMBOL:
+        DataTransfer = ONE_LEVEL_INTERVAL_NUM;
+        break;
+    case ZERO_SYMBOL:
+        DataTransfer = ZERO_LEVEL_INTERVAL_NUM;
+        break;
+    }
+
+    HAL_TIM_OC_Start(&htim2, TIM_CHANNEL_1);
+    HAL_TIM_OC_Start_IT(&htim21, TIM_CHANNEL_1);
+    // wait for end transfer of symbol
+    while (DataTransfer){
+
+    }
+    HAL_TIM_OC_Stop(&htim2, TIM_CHANNEL_1);
+
+    // start transfer separate symbol
+    DataTransfer = PAUSE_LEVEL_INTERVAL_NUM;
+
+    while (DataTransfer){
+
+    }
+    HAL_TIM_OC_Stop(&htim21, TIM_CHANNEL_1);
+    return 0;
+}
+
+uint32_t CreasteIrPacket (IR_PacketTypeDef IR_Packet)
+{
+    uint32_t Packet = 0;
+    Packet = IR_Packet.SendIrAddr;
+    Packet = Packet | (IR_Packet.SendIrCommand << ADDR_LENGTH);
+    return Packet;
+}
+
+
